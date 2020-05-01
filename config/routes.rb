fcb70c5911a7762a450cdf4fd9bc2b8f1ee4369a
github username <github email address>
2020-04-30 20:14:44 -0400

    added sign in routes and sign in form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # get 'welcome/home'
   root 'welcome#home'
+  get '/signup' => 'users#new'
+  get '/signin' => 'welcome#new'
+  post '/signin' => 'welcome#create'
   resources :charts
   resources :meals
   resources :users
