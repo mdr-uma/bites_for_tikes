e3832679a4df2f41b20e824fa76956039018cce6
github username <github email address>
2020-05-01 18:30:11 -0400

    added new, create and meal_params method in meals controller.

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -3,4 +3,21 @@ class MealsController < ApplicationController
         @meal = Meal.new
     end
 
+    def show
+        @meal = Meal.find(params[:id])
+    end
+
+    def create
+        @meal = Meal.new(meal_params)
+        if @meal.save
+            redirect_to meal_path(@meal)
+        end
+    end
+
+    private
+
+    def meal_params
+        params.require(:meal).permit(:meal_name, :meal_type, :note)
+    end
+
 end
