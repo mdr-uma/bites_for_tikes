0f6796b31916173adc48dc942311d76bfacedafa
github username <github email address>
2020-05-01 18:20:39 -0400

    added link to create meal in chart show page, created new file in meal view and def create and chart_params method in charts controller.

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -2,4 +2,23 @@ class ChartsController < ApplicationController
     def new
         @chart = Chart.new
     end
+
+    def show
+        @chart = Chart.find(params[:id])
+    end
+
+    def create
+        @chart = Chart.new(chart_params)
+        if @chart.save
+            redirect_to chart_path(@chart)
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def chart_params
+        params.require(:chart).permit(:date, :days, :time)
+    end
 end
