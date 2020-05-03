df7f17e9ce63467125cb9227e305fc8ee4a11db8
github username <github email address>
2020-05-03 15:06:29 -0400

    added some validations in user model, updated about index file, tiny changes to users show page and css.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
     
     has_many :meals, dependent: :destroy
     has_many :charts, through: :meals
+
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true
+    validates :password, length: { in: 6..20 }
+
+
 end
