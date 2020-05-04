8ccd5d2afa092ed4a3d501460f71a89a1ab3f071
github username <github email address>
2020-05-04 19:17:16 -0400

    defined current user, logged_in? and require_logged_in method in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+    def current_user
+        @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+
+    def logged_in?
+        !!session[:user_id]
+    end
+
+    def require_logged_in
+        return redirect_to signin_path unless logged_in?
+    end
 end
