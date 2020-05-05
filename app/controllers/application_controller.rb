eb6652f761d597703782e81fd9bf7ca182736cfe
github username <github email address>
2020-05-05 16:35:55 -0400

    created helper method current_user, logged_in? , required_logged_in and added condition in nav bar who can view it, and created errors partial page in partials folder

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user, :logged_in?, :require_logged_in
     def current_user
         @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
     end
