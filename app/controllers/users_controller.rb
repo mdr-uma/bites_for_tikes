44ffd960965434120c9d8688e7b5b9bcb06d78ec
github username <github email address>
2020-05-06 12:33:41 -0400

    moved errors file to layout folder, break signup form into 3 partial, and render it to new form of users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
             session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
-            redirect_to root_path
+            render :new 
         end
     end
 
