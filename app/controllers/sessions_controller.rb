1a195c3095559583f4010429b76830d48cdd2539
github username <github email address>
2020-05-05 22:53:59 -0400

    changeed redirect path to create action in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,9 @@ class SessionsController < ApplicationController
 
     def create
         @user = User.find_by(email: params[:user][:email])
-        # binding.pry
         if @user && @user.authenticate(params[:user][:password])
             session[:user_id] = @user.id
-            redirect_to new_chart_path
+            redirect_to user_path(@user)
         else
             redirect_to signin_path
         end
