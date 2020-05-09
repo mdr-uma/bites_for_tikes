d0899853cc5fac11cdcba385de4b4c0cbf70ea65
github username <github email address>
2020-05-08 22:32:36 -0400

    added auth/facebook route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   post '/signin' => 'sessions#create'
   delete '/signout' => 'sessions#destroy'
   get 'about' => 'about#index'
+  get 'auth/facebook/callback' => 'sessions#create'
   
   resources :charts do 
     resources :meals, only: [:new, :show, :index]
