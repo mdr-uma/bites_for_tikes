13abab76ff6a5a9009328e871accf3c70f8f04eb
github username <um_2003_8@hotmail.com>
2020-05-15 19:40:53 -0400

    added validation

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -2,5 +2,5 @@ class Meal < ApplicationRecord
     belongs_to :user, optional: true
     belongs_to :chart, optional: true
 
-    validates :meal_name, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
+    validates :meal_name, presence: true, format: { with: /\A[a-zA-Z\s]+\z/, message: "only allows letters" }
 end
