648d0da18cb70265c08f4ee269c7b2a01b1aaffb
github username <um_2003_8@hotmail.com>
2020-06-09 22:39:57 -0400

    update scope method

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -4,7 +4,7 @@ class Meal < ApplicationRecord
 
     validates :meal_name, presence: true, format: { with: /\A[a-zA-Z\s]+\z/, message: "only allows letters" }
 
-     scope :meals_by_meal_type, -> (name) {where("meal_type == ?", name)}
-     scope :meals_by_user, -> (name) {where("user_id == ?", name)}
+     scope :meals_by_meal_type, -> (name) {where(meal_type: name)}
+     scope :meals_by_user, -> (name) {where(user_id: name)}
     
 end
