ccabf343bdd86e7004b7b3a6a7bacefd0718ad45
github username <um_2003_8@hotmail.com>
2022-02-19 12:09:39 -0500

    add break lines to make it more readable

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,21 @@
 Rails.application.routes.draw do
   root 'welcome#home'
+  get 'about' => 'about#index'
+
   get '/signup' => 'users#new'
   get '/signin' => 'sessions#new'
   post '/signin' => 'sessions#create'
   delete '/signout' => 'sessions#destroy'
-  get 'about' => 'about#index'
+
   get 'auth/facebook/callback' => 'sessions#create'
   get 'auth/github/callback' => 'sessions#create'
+
   get '/search' => 'meals#search'
 
-  
   resources :charts do 
     resources :meals, only: [:new, :show, :index]
   end
+
   resources :meals
   resources :users, only: [:new, :create, :show]
 end
